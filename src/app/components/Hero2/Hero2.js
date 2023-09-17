b663becd7c0718cdd730586e33d67c6943f27f21
Hugo Moreira <hugo.n4@gmail.com>
2023-09-17 09:38:58 -0700

    update hero

--- src/app/components/Hero2/Hero2.js
+++ src/app/components/Hero2/Hero2.js
@@ -7,10 +7,6 @@ import Image from 'next/image'
 
 
 const navigation = [
-  { name: 'Contact', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Blog', href: '#' },
-  { name: 'Company', href: '#' },
   { name: 'Log in', href: 'https://www.neurafox.com/sign-in', target:"_parent"},
 ]
 
@@ -19,33 +15,32 @@ export default function Example() {
 
   return (
     <div className="bg-white">
-      <header className="absolute inset-x-0 top-0 z-50">
-        <div className="mx-auto max-w-7xl">
-          <div className="px-6 pt-6 lg:max-w-2xl lg:pl-8 lg:pr-0">
-            <nav className="flex items-center justify-between lg:justify-start" aria-label="Global">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">NeuraFox</span>
-                <Image src="/images/logo03.png" alt="NeuraFox Logo" width={64} height={150} />
-
-              </a>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700 lg:hidden"
-                onClick={() => setMobileMenuOpen(true)}
-              >
-                <span className="sr-only">Open main menu</span>
-                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-              </button>
-              <div className="hidden lg:ml-12 lg:flex lg:gap-x-14">
-                {navigation.map((item) => (
-                  <a key={item.name} href={item.href} target={item.target} className="text-sm font-semibold leading-6 text-gray-900">
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </nav>
-          </div>
+    <header className="absolute inset-x-0 top-0 z-50">
+      <div className="mx-auto max-w-7xl">
+        <div className="px-6 pt-6 lg:max-w-2xl lg:pl-8 lg:pr-0">
+          <nav className="flex items-center justify-between lg:justify-between" aria-label="Global">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">NeuraFox</span>
+              <Image src="/images/logo03.png" alt="NeuraFox Logo" width={64} height={150} />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700 lg:hidden"
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+            <div className="hidden lg:ml-12 lg:flex lg:gap-x-14 lg:ml-auto">
+              {navigation.map((item) => (
+                <a key={item.name} href={item.href} target={item.target} className="text-sm font-semibold leading-6 text-gray-900">
+                  {item.name}
+                </a>
+              ))}
+            </div>
+          </nav>
         </div>
+      </div>
         <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
